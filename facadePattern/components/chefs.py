21d28ee8fb9a3de35d9eb02d95f2aac81376ac8f
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-01-09 22:07:23 +0800

    add HappyBreakfast chef

---
+++ facadePattern/components/chefs.py
@@ -0,0 +1,8 @@
+class HappyBreakfastShopChef():
+    def prepare(self, meal):
+        """ Implement prepare behavior """
+        print('chef prepare for meal {}'.format(meal))
+
+    def cook(self, meal):
+        """ Implement cook behavior """
+        print('chef cook meal {}'.format(meal))
