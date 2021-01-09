0c31d5e826978a0e7cb6730230cf76746c97e9f1
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-01-09 22:08:16 +0800

    add HappyBreakfast waiter

---
+++ facadePattern/components/waiters.py
@@ -0,0 +1,12 @@
+class HappyBreakfastShopWaiter():
+    def order(self, meal):
+        """ Implement order behavior """
+        print('order meal: {}'.format(meal))
+
+    def clean(self):
+        """ Implement clean behavior """
+        print('clean the table')
+
+    def serve(self, meal):
+        """ Implement serve behavior """
+        print('serve the meal {}'.format(meal))
