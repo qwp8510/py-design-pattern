3484632a31f2e06325332970312bf7c17a80d7f9
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-01-18 23:59:26 +0800

    add MenuComponent

---
+++ compositePattern/menu_component.py
@@ -0,0 +1,21 @@
+class MenuComponent():
+    def add_child(self, menu_component):
+        pass
+
+    def remove_child(self, menu_component):
+        pass
+
+    def get_child(self, idx):
+        pass
+
+    def get_item_name(self):
+        pass
+
+    def get_price(self):
+        pass
+
+    def get_description(self):
+        pass
+
+    def show(self):
+        pass
