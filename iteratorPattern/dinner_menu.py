a70244be590b8f90d76154ccdc7bfb6eb11a8e8d
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-01-18 14:27:37 +0800

    add dinner and lunch menu

---
+++ iteratorPattern/dinner_menu.py
@@ -0,0 +1,16 @@
+from menu_item import MenuItem
+from iterators import DinnerIterator
+
+
+class DinnerMenu():
+    def __init__(self):
+        self._menu_items = {}
+        self.add_item('salmon', '340', 'pan-fried salmon')
+        self.add_item('whole wheat bread', '50', 'for vegetarian')
+
+    def add_item(self, name, price, description):
+        self._menu_items[name] = MenuItem(name, price, description)
+
+    @property
+    def item(self):
+        return DinnerIterator(self._menu_items)
---
+++ iteratorPattern/lunch_menu.py
@@ -0,0 +1,16 @@
+from menu_item import MenuItem
+from iterators import LunchIterator
+
+
+class LunchMenu():
+    def __init__(self):
+        self._menu_items = []
+        self.add_item('beef', '240', 'pan-fried steak')
+        self.add_item('white bread', '50', 'for vegetarian')
+
+    def add_item(self, name, price, description):
+        self._menu_items.append(MenuItem(name, price, description))
+
+    @property
+    def item(self):
+        return LunchIterator(self._menu_items)
