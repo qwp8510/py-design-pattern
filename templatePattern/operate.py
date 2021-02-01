07a480c394b98b06e4c042f94c88a5732810c591
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-02-01 22:44:26 +0800

    add order function into DrinkMachine for template pattern

--- templatePattern/operate.py
+++ templatePattern/operate.py
@@ -4,16 +4,22 @@ from drink import Coco, Coffee
 class DrinkMachine():
     DRINK = {'coco': Coco, 'coffee': Coffee}
 
-    def __init__(self, drink_type):
-        self.drink = self.DRINK.get(drink_type)()
+    def __init__(self):
+        self._drink = None
+
+    def order(self, drink_type):
+        self._drink = self.DRINK.get(drink_type)()
 
     def press_prepare_button(self):
-        self.drink.prepare()
+        self._drink.prepare()
 
 
 def main():
-    DrinkMachine('coco').press_prepare_button()
-    DrinkMachine('coffee').press_prepare_button()
+    drink_machine = DrinkMachine()
+    drink_machine.order('coco')
+    drink_machine.press_prepare_button()
+    drink_machine.order('coffee')
+    drink_machine.press_prepare_button()
 
 
 if __name__ == '__main__':
