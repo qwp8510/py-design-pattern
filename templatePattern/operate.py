7696286efcc2496fb9fea67771e8bdd45f0e5e12
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-02-01 21:55:25 +0800

    Fix for template pattern, add DrinkMachine prepare for drink.

--- templatePattern/operate.py
+++ templatePattern/operate.py
@@ -1,9 +1,19 @@
 from drink import Coco, Coffee
 
 
+class DrinkMachine():
+    DRINK = {'coco': Coco, 'coffee': Coffee}
+
+    def __init__(self, drink_type):
+        self.drink = self.DRINK.get(drink_type)()
+
+    def press_prepare_button(self):
+        self.drink.prepare()
+
+
 def main():
-    Coco().prepare()
-    Coffee().prepare()
+    DrinkMachine('coco').press_prepare_button()
+    DrinkMachine('coffee').press_prepare_button()
 
 
 if __name__ == '__main__':
