668de5298f180ecc633a0643bfa0a54d76ae9abd
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-02-01 21:54:41 +0800

    Fix for template pattern, extract base class VendingMachine to drink and rename to BaseDrink

--- templatePattern/drink.py
+++ templatePattern/drink.py
@@ -1,15 +1,44 @@
-from drink_vending_machine import HotCocoVendingMachine
+from abc import ABC, abstractmethod
 
 
-class Coco(HotCocoVendingMachine):
+class BaseDrink(ABC):
+    def prepare(self):
+        self.add_powder()
+        self.brew()
+        self.pour_into_cup()
+        if self.want_condiments():
+            self.add_condiments()
+
+    @abstractmethod
+    def add_powder(self):
+        pass
+
+    def brew(self):
+        print('steeping')
+
+    def pour_into_cup(self):
+        print('pour drink into cup')
+
+    @abstractmethod
+    def add_condiments(self):
+        pass
+
+    def want_condiments(self):
+        return True
+
+
+class Coco(BaseDrink):
     def add_powder(self):
         print('add coco powder')
 
     def add_condiments(self):
         print('add nothing')
 
+    def want_condiments(self):
+        return False
 
-class Coffee(HotCocoVendingMachine):
+
+class Coffee(BaseDrink):
     def add_powder(self):
         print('add coffee powder')
 
